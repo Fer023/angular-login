7201a2466d8729b6b33b0e442665899d4e239a95
Interituss <ferrochaca@gmail.com>
2025-05-13 12:12:27 -0300

    Java Spring Auth

--- login-page/src/app/services/login.service.ts
+++ login-page/src/app/services/login.service.ts
@@ -2,17 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/login-response.type';
 import { tap } from 'rxjs/operators';
+import { environment } from 'environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private apiUrl = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) { }
 
   login (name: string, password: string) {
-    return this.httpClient.post<LoginResponse>('/login', {name, password}).pipe(
+    return this.httpClient.post<LoginResponse>(`${this.apiUrl}/auth/login`, {name, password}).pipe(
       tap((value) => {
         sessionStorage.setItem("authToken", value.token)
         sessionStorage.setItem("username", value.name)
@@ -20,3 +22,4 @@ export class LoginService {
     )
   }
 }
+
